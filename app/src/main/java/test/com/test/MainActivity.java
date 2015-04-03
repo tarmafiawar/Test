c962760d45082418b69c9b56b52e3f4d5e319db4
taro <tarmafiawar@gmail.com>
2015-04-03 21:05:18 +0700

    Implement get json from url.

--- app/src/main/java/test/com/test/MainActivity.java
+++ app/src/main/java/test/com/test/MainActivity.java
@@ -1,15 +1,29 @@
 package test.com.test;
 
-import android.app.Application;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -31,8 +45,11 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(context, text, duration);
                 Log.v(TAG, "test : ");
 
+                new ConnectNetworkTask().execute();
+
             }
         });
+
     }
 
 
@@ -56,5 +73,73 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
+class ConnectNetworkTask extends AsyncTask<String, Void, String> {
+
+    private Exception exception;
+    private static final String TAG = "MyActivity";
+
+    protected String doInBackground(String... urls) {
+        try {
+
+            HttpGet httpGet = new HttpGet("http://ip.jsontest.com/");
+            HttpClient client = new DefaultHttpClient();
+            try {
+                HttpResponse response = client.execute(httpGet);
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    InputStream instream = entity.getContent();
+                    String result = convertStreamToString(instream);
+                    instream.close();
+                    Log.v(TAG, "result : " +result);
+                }
+            } catch (ClientProtocolException e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            }catch (UnknownHostException e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            }catch (ConnectTimeoutException e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            } catch (IOException e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            } catch (Exception e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            }
+
+            return "";
+        } catch (Exception e) {
+            this.exception = e;
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String s) {
+        // TODO: check this.exception
+        // TODO: do something with the feed
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                Log.e("SHIMDEE_LIBS", e.getMessage(), e);
+            }
+        }
+        return sb.toString();
     }
 }
